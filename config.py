6aab87f884f64ac3db1d80a493779f7c0d7df8f2
chichan44 <74105348+chichan44@users.noreply.github.com>
2024-01-19 01:21:16 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+def getToken():
+  return "ODI0ODI1MDc1NzEwNjg5Mjkx.GZHX8I.izIvABIHLpcScMpPVAqK2RBk4GN5LBrPSvxNXM"
+
+def getOpenAIKey():
+  return "sk-ObpuWebGAVMzAPtTKBmoT3BlbkFJ5d6swd6quXWpn59mXw0ce"
+
+def getOwnerUsername():
+  return "upio"
