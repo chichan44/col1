5e7609414cfd1604f5f62149d66e9474e56336b5
chichan44 <74105348+chichan44@users.noreply.github.com>
2024-01-19 01:24:49 +0700

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-def getToken():
-  return "ODI0ODI1MDc1NzEwNjg5Mjkx.GZHX8I.izIvABIHLpcScMpPVAqK2RBk4GN5LBrPSvxNXM"
-
-def getOpenAIKey():
-  return "sk-ObpuWebGAVMzAPtTKBmoT3BlbkFJ5d6swd6quXWpn59mXw0ce"
-
-def getOwnerUsername():
-  return "upio"
